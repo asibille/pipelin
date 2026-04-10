669146839a99d76b2c00bfdfd3049cf32f5c3dee
sibille <axsibille@gmail.com>
2026-04-10 16:09:48 +0200

    Premier commit

---
+++ function direBonjour(nom) {.js
@@ -0,0 +1,20 @@
+function direBonjour(nom) {
+    console.log("Bonjour, " + nom + " !");
+}
+
+// Appel de la fonction
+direBonjour("Alice");
+
+// Test avec une boucle
+for (let i = 1; i <= 5; i++) {
+    console.log("Compteur :", i);
+}
+
+// Test conditionnel
+let nombre = 10;
+
+if (nombre > 5) {
+    console.log("Le nombre est supérieur à 5");
+} else {
+    console.log("Le nombre est inférieur ou égal à 5");
+}
